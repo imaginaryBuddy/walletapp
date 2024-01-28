cd2646fb400c8a48b7c8cc5d00a856e54d01b812
Shen Hwei Yap <81760484+imaginaryBuddy@users.noreply.github.com>
2024-01-29 00:46:09 +0800

    Add vercel configs

--- server/server.js
+++ server/server.js
@@ -7,11 +7,23 @@ const transactionsRoute = require("./routes/transactionRoute");
 const requestsRoute = require("./routes/requestsRoute");
 
 const PORT = process.env.PORT || 5000;
+const path = require("path"); 
+
+
 
 app.use("/api/users", usersRoute);
 app.use("/api/transactions", transactionsRoute);
 app.use("/api/requests", requestsRoute);
 
+app.use(cors(
+  {
+    origin: ["https://walletapp-jet.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true, 
+  }
+));
+
+mongoose.connect('')
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
